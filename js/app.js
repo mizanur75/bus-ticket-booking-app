11eedc03ddce27c259571a38aa82997b498b46b9
Mizanur Rahman <36396771+mizanur75@users.noreply.github.com>
2023-04-03 11:33:17 +0600

    seat array create

--- js/app.js
+++ js/app.js
@@ -23,7 +23,149 @@ var app = Vue.createApp({
                     }
                 ]
             },
-            count: 0
+            count: 0,
+            seats:[
+                {
+                    name:'A1',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'A2',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'A3',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'A4',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'B1',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'B2',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'B3',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'B4',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'C1',
+                    type:'booked',
+                    price: 500
+                },
+                {
+                    name:'C2',
+                    type:'sold',
+                    price: 500
+                },
+                {
+                    name:'C3',
+                    type:'booked',
+                    price: 500
+                },
+                {
+                    name:'C4',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'D1',
+                    type:'sold',
+                    price: 500
+                },
+                {
+                    name:'D2',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'D3',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'D4',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'E1',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'E2',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'E3',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'E4',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'F1',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'F2',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'F3',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'F4',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'G1',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'G2',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'G3',
+                    type:'available',
+                    price: 500
+                },
+                {
+                    name:'G4',
+                    type:'available',
+                    price: 500
+                }
+            ]
         };
     },
     methods:{
